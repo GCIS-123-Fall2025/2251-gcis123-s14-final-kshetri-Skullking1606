45040d414526749b436a4689d5ede8a4af527f25
Elijah DeLeon (RIT Student) <end7333@rit.edu>
2025-12-15 08:42:17 -0500

    Question 1 Implementation

--- shoppers.py
+++ shoppers.py
@@ -18,11 +18,42 @@ Write down the manual test by creating at least two items.
 """
 
 class Item:
-    pass # please replace with your solution
+    __slots__ = ['__item_code','__name','__price']
+
+    def __init__(self,item_code,name,price):
+        self.__item_code = item_code
+        self.__name = name
+        self.__price = price
+        
+    def get_item_code(self):
+        return self.__item_code
+
+    def get_name(self):
+        return self.__name
+    
+    def get_price(self):
+        return self.__price
+    
+    def __str__(self):
+        return "("+ self.__item_code +","+ self.__name +","+ str(self.__price) +")"
+
+    def __eq__(self, value):
+        if type(value) == type(self):
+            if self.__item_code == value.get_item_code(): return True
+        return False
+    
+    def __hash__(self):
+        return hash(self) # Was not taught formally; Best Guess given.
+
+# Missing sort Method - Unsure on that one. Partial working code :)
 
 
 # manual test from main() method
-def main():     pass
+def main():     
+    my_item_one = Item('AEH-YU','R.A.M.',9999)
+    print(my_item_one)
+    my_item_two = Item('GEH-SUM','Lettuce', 27)
+    print(my_item_two)
  
 
 if __name__ == "__main__":    main()
